8263d2adb51f5d9f2bdb2a136c79c23cde823ba0
Luan Accioly <acciolyluan@gmail.com>
2023-11-29 23:17:42 -0300

    refactor: update initialColorMode in theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,8 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const config = {
-  initialColorMode: 'light',
   useSystemColorMode: true,
-  disableTransitionOnChange: false,
+  initialColorMode: 'light',
 };
 
 const colors = {
