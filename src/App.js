f88f7df03cd1f831fb6c5c0bd0d5541ea3f56bb2
Luan Accioly <acciolyluan@gmail.com>
2023-11-30 00:19:56 -0300

    feat: add routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './styles/theme';
-import { Newsletter } from './Pages/Newsletter';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Routes } from './routes';
 
 function App() {
+  const router = createBrowserRouter(Routes);
   return (
     <ChakraProvider theme={theme}>
-      <Newsletter />
+      <RouterProvider router={router} />
     </ChakraProvider>
   );
 }
